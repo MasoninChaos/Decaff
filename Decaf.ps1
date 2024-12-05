f8aff8c123d4fbad3343586b2b205dd50b6a7408
Mason <mason.wright@roadchef.com>
2024-12-05 10:23:38 +0000

    Delete Decaf.ps1

--- Decaf.ps1
+++
@@ -1,7 +0,0 @@
-﻿$javaVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "Enter Program Name Here" } | Select-Object -Property DisplayName, UninstallString
-ForEach ($ver in $javaVer) {
-	If ($ver.UninstallString) {
-		$uninst = $ver.UninstallString
-		Start-Process cmd -ArgumentList "/c $uninst /quiet /norestart" -Verb RunAs -Wait
-		}
-	}
